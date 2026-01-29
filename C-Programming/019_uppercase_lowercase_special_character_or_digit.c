1c8d202744bf57238595907c3700a7f365843b4a
Sagar Sahu <sagar.sahu2023@ssipmt.com>
2026-01-29 21:45:08 +0530

    Add files via upload

---
+++ C-Programming/019_uppercase_lowercase_special_character_or_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+    if(ch >= 'a' && ch <= 'z')
+        printf("%c is lowercase", ch);
+    else if(ch >= 'A' && ch <= 'Z')
+        printf("%c is uppercase", ch);
+    else if(ch >= '0' && ch <= '9')
+        printf("%c is a digit", ch);
+    else
+        printf("%c is a special character", ch);
+    return 0;
+}
