8710b62c2bd21ba9179a0aff5ec2910e1d470b2f
Sagar Sahu <sagar.sahu2023@ssipmt.com>
2026-02-06 20:27:51 +0530

    Modify GCD program to handle negative integers
    Added handling for negative inputs by using abs() function.

--- C++/gcd_of_two_numbers.cpp
+++ C++/gcd_of_two_numbers.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-#include <cstdio>
 using namespace std;
 int main() {
     int n1, n2;
-    cout << "Enter two positive integers: ";
+    cout << "Enter two integers: ";
     cin >> n1 >> n2;
+    n1 = abs(n1); // Handle negative inputs
+    n2 = abs(n2);
     while(n1 != n2) {
-        if(n1 > n2)
-            n1 -= n2;
-        else
-            n2 -= n1;
+        if(n1 > n2) n1 -= n2;
+        else n2 -= n1;
     }
     cout << "GCD = " << n1;
     return 0;
