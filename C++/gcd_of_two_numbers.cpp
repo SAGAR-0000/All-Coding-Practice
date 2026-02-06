fe4ffd6659c9c75ff1ff257558848805c98489f9
Sagar Sahu <sagar.sahu2023@ssipmt.com>
2026-02-06 17:50:33 +0530

    Add files via upload

---
+++ C++/gcd_of_two_numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main() {
+    int n1, n2;
+    cout << "Enter two positive integers: ";
+    cin >> n1 >> n2;
+    while(n1 != n2) {
+        if(n1 > n2)
+            n1 -= n2;
+        else
+            n2 -= n1;
+    }
+    cout << "GCD = " << n1;
+    return 0;
+}
