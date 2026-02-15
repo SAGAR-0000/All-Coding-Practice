adb8db273dfb909cb95096adb6ee30c19fcf51c7
Sagar Sahu <sagar.sahu2023@ssipmt.com>
2026-02-15 19:51:48 +0530

    Add files via upload

---
+++ C++/fibonacci_recursion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int fib(int n) {
+    if (n <= 1) return n;
+    return fib(n - 1) + fib(n - 2);
+}
+int main() {
+    int n, i;
+    cout << "Enter number of terms: ";
+    cin >> n;
+    cout << "Fibonacci Series: ";
+    for(i = 0; i < n; i++) {
+        cout << fib(i) << " ";
+    }
+    return 0;
+}
---
+++ C++/gcd_recursion.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int hcf(int n1, int n2) {
+    if (n2 != 0)
+        return hcf(n2, n1 % n2);
+    else
+        return n1;
+}
+int main() {
+    int n1, n2;
+    cout << "Enter two integers: ";
+    cin >> n1 >> n2;
+    cout << "G.C.D of " << n1 << " and " << n2 << " is " << hcf(n1, n2);
+    return 0;
+}
