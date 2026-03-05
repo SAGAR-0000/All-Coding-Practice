32e0ef39541ebed55416788f6a0da9cd0b4ccd6d
Sagar Sahu <sagar.sahu2023@ssipmt.com>
2026-03-05 22:33:59 +0530

    Add files via upload

---
+++ C-Programming/056_check_whether_an_alphabet_is_a_vowel_or_consonant_using_a_switch_case.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("Enter an alphabet: ");
+    scanf(" %c", &ch);
+    switch(ch) {
+        case 'a': case 'A':
+        case 'e': case 'E':
+        case 'i': case 'I':
+        case 'o': case 'O':
+        case 'u': case 'U':
+            printf("%c is a vowel", ch);
+            break;
+        default:
+            if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+                printf("%c is a consonant", ch);
+            else
+                printf("Not an alphabet");
+    }
+    return 0;
+}
