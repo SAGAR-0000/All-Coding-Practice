222173d1b8d771a1c518223b9ff7211b3f9f9d72
Sagar Sahu <sagar.sahu2023@ssipmt.com>
2026-03-08 18:08:30 +0530

    Add files via upload

---
+++ C-Programming/058_check_whether_a_number_is_even_or_odd_using_a_switch_case.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    switch(num % 2) {
+        case 0:
+            printf("%d is even", num);
+            break;
+        case 1:
+            printf("%d is odd", num);
+            break;
+    }
+    return 0;
+}
