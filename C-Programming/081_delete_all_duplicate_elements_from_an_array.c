3e5ac8f5580e1f01081bb306082284e4540598e3
Sagar Sahu <sagar.sahu2023@ssipmt.com>
2026-04-02 19:57:23 +0530

    Add files via upload

---
+++ C-Programming/081_delete_all_duplicate_elements_from_an_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+    int arr[100], temp[100], i, j, k = 0, n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    printf("Enter elements in array: ");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for(i = 0; i < n; i++) {
+        int isDuplicate = 0;
+        for(j = 0; j < k; j++) {
+            if(arr[i] == temp[j]) {
+                isDuplicate = 1;
+                break;
+            }
+        }
+        if(!isDuplicate) {
+            temp[k] = arr[i];
+            k++;
+        }
+    }
+    printf("Array after removing duplicates: ");
+    for(i = 0; i < k; i++) {
+        printf("%d ", temp[i]);
+    }
+    return 0;
+}
