41f7055cc78692c001092e5f722ebf0bba0659b2
Sagar Sahu <sagar.sahu2023@ssipmt.com>
2026-04-08 20:52:06 +0530

    Add files via upload

---
+++ C++/p2.cpp
@@ -0,0 +1,12 @@
+//half the input
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter No : "<<endl;
+    cin>>a;
+    float b = (float)a;
+    cout<<"Half is : "<<b/2;
+    return 0;
+}
