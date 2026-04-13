bd7f5248154ca5c2e217100157b46e21be0850c8
Sagar Sahu <sagar.sahu2023@ssipmt.com>
2026-04-13 21:56:05 +0530

    Add files via upload

---
+++ C-Programming/089_rightrotate_an_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int arr[100], i, n, rotation, temp;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    printf("Enter elements in array: ");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter number of rotations: ");
+    scanf("%d", &rotation);
+    for(i = 0; i < rotation; i++) {
+        temp = arr[n - 1];
+        for(j = n - 1; j > 0; j--) {
+            arr[j] = arr[j - 1];
+        }
+        arr[0] = temp;
+    }
+    printf("Array after right rotation: ");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
