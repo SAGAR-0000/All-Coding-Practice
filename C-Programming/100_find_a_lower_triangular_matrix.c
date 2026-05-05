688bbe894733677bcfdbbcd81cf6b248543bb682
Sagar Sahu <sagar.sahu2023@ssipmt.com>
2026-05-05 20:18:18 +0530

    Add files via upload

---
+++ C-Programming/100_find_a_lower_triangular_matrix.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define MAX 10
+int main() {
+    int mat[MAX][MAX], i, j, rows, cols;
+    printf("Enter number of rows and columns (must be square): ");
+    scanf("%d %d", &rows, &cols);
+    if(rows != cols) {
+        printf("Not a square matrix");
+        return 0;
+    }
+    printf("Enter elements of matrix:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+    printf("Lower triangular matrix:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            if(j <= i)
+                printf("%d ", mat[i][j]);
+            else
+                printf("0 ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
