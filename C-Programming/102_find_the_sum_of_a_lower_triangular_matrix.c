00923dbb7dde6190f7ef1a414fd08ba18bcfb75d
Sagar Sahu <sagar.sahu2023@ssipmt.com>
2026-05-06 20:14:33 +0530

    Add files via upload

---
+++ C-Programming/102_find_the_sum_of_a_lower_triangular_matrix.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define MAX 10
+int main() {
+    int mat[MAX][MAX], i, j, rows, cols, sum = 0;
+    printf("Enter number of rows and columns (must be square): ");
+    scanf("%d %d", &rows, &cols);
+    if(rows != cols) {
+        printf("Not a square matrix");
+        return 0;
+    }
+    printf("Enter elements of matrix:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            scanf("%d", &mat[i][j]);
+            if(j <= i)
+                sum += mat[i][j];
+        }
+    }
+    printf("Sum of lower triangular matrix: %d", sum);
+    return 0;
+}
