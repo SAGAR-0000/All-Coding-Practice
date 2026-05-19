f0e51e098b2032314065b2b4f94e855bd227f658
Sagar Sahu <sagar.sahu2023@ssipmt.com>
2026-05-19 20:22:10 +0530

    Leetcode2540

--- Python-Scripts/Leetcode_2540.py
+++ Python-Scripts/Leetcode_2540.py
@@ -4,6 +4,7 @@ class Solution:
         j = 0
         n1 = len(nums1)
         n2 = len(nums2)
+        
         while i < n1 and j < n2:
             if nums1[i] == nums2[j]:
                 return nums1[i]
