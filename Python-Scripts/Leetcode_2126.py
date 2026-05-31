8d2c8010ff167130cc76816fb2d6382a8f9a0c6b
Sagar Sahu <sagar.sahu2023@ssipmt.com>
2026-05-31 17:43:45 +0530

    Leetcode 2126

---
+++ Python-Scripts/Leetcode_2126.py
@@ -0,0 +1,11 @@
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: list[int]) -> bool:
+        asteroids.sort()
+        currentmass = mass
+        
+        for asteroidmass in asteroids:
+            if asteroidmass > currentmass:
+                return False
+            currentmass += asteroidmass
+
+        return True
